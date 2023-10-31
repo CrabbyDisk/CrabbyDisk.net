05868140b33623ccac6ac7b98380c74ef2c844d9
Crabby Disk <yfmao2010@hotmail.com>
2023-10-30 20:53:55 -0400

    Update

--- src/routes/compare/+page.svelte
+++ src/routes/compare/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     let cardList = [
     {
         ammount: 1,
@@ -9,27 +9,24 @@
         ammount: 1,
         price: 1,
         ratio: 1
-    }
-    ]
+    }]
 
-
+    let best: number;
 
     function addCard() {
         cardList = [...cardList, {ammount: 1, price: 1, ratio: 1}];
     }
 
-    function removeCard(index) {
+    function removeCard(index: number) {
         cardList.splice(index, 1)
         cardList = cardList;
     }
 
-
-    function changeRatio(index) {
+    function changeRatio(index: number) {
         cardList[index].ratio = Math.round(cardList[index].ammount / cardList[index].price * 100) / 100;
         compare();
     }   
     function compare() {
-        let best = 0;
         for(let i in cardList) {
             if(cardList[best].ratio < cardList[i].ratio) {
                 best = i
@@ -51,6 +48,9 @@
             <p>Price</p>
             <input bind:value={card.price} on:input={() => changeRatio(index)} type="number" min="0" max="1000000" />
             <p>{card.ratio} {unit} per $</p>
+            {#if best == index}
+                <div class="best-card"><p>The Best</p></div>
+            {/if}
         </article>
     {/each}
 
@@ -71,5 +71,8 @@
     }
     .compare-card {
         margin: 0;
+    }
+    .best-card {
+        color: green;
     }
 </style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import adapter from '@sveltejs/adapter-vercel';
- 
-export default {
-  kit: {
-    adapter: adapter({
-        runtime: 'nodejs18.x',
-      }),
-  },
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
